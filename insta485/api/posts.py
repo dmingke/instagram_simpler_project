f45896536074ddf7adf374d1da01c14398fe14ee
dmingke <73776136+dmingke@users.noreply.github.com>
2023-02-08 17:50:46 -0500

    Update posts.py

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -2,6 +2,77 @@
 import flask
 import insta485
 
+@insta485.app.route('/api/v1/', methods=["GET"])
+def get_services():
+    # Return a list of services available. The output 
+    # should look exactly like this example. 
+    # Does not require user to be authenticated.
+    context = {
+        "comments": "/api/v1/comments/",
+        "likes": "/api/v1/likes/",
+        "posts": "/api/v1/posts/",
+        "url":  flask.request.full_path
+    }
+    return flask.jsonify(**context), 200
+
+@insta485.app.route('/api/v1/posts/', methods=["GET"])
+def get_post():
+    # Return the 10 newest posts.
+    # each post is made by a user which the logged in user follows 
+    # or the post is made by the logged in user. 
+    # When postid_lte is not specified, default to the most recent postid
+    logname = flask.session.get('username')
+    if not logname:
+        logname = flask.request.authorization['username']
+        password = flask.request.authorization['password']
+        if not logname or not password:
+            return flask.jsonify({}), 400
+    size = flask.request.args.get("size", default=10, type=int)
+    # size_if_user = flask.request.args.get("size", None)
+    page = flask.request.args.get("page", default=0, type=int)
+    # page_if_user = flask.request.args.get("page", None)
+    if size < 0 or page < 0:
+        return flask.jsonify({}), 400
+    connection = insta485.model.get_db()
+    total_postid = connection.execute(
+        "SELECT postid FROM posts "
+        "where owner == ? OR "
+        "owner IN (SELECT username2 FROM following "
+        "where username1 == ?)",
+        (logname, logname, )
+    ).fetchall()
+    numPost = len(total_postid)
+    newest = total_postid[numPost-1]['postid']
+    postid_lte = flask.request.args.get("postid_lte", default=newest, type=int)
+    # postid_if_user = flask.request.args.get("page"
+    selected_postid = connection.execute(
+        "SELECT postid,  ('/api/v1/posts/' || postid || '/') AS url FROM posts "
+        "where owner == ? OR "
+        "owner IN (SELECT username2 FROM following "
+        "where username1 == ?) ORDER BY postid DESC "
+        "LIMIT ? OFFSET ?",
+        (logname, logname, size, size*page,)
+    ).fetchall()
+    # if not size_if_user and not page_if_user:
+
+    if postid_lte == newest:
+        if size*(page+1) <= numPost:
+            # ?????????what is the page (the max)
+            next = "/api/v1/posts/?size={}&page={}&postid_lte={}".format(size, page+1,newest)
+        else:
+            next = ""
+    else:
+        next = "/api/v1/posts/?size={}&page={}&postid_lte={}".format(size, page+1,postid_lte)
+    results = list(selected_postid)
+
+    context = {
+        "next": next,
+        "results": results,
+        "url": flask.request.full_path.rstrip("?"),
+    }
+    return flask.jsonify(**context), 200
+
+
 @insta485.app.route('/api/v1/posts/<int:postid_url_slug>/', methods=['GET'])
 def get_post(postid_url_slug):
     #checking authorization...
