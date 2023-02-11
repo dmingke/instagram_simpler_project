7d4de8f45118a448478ab5f2cd0e7484a2fbbee5
dmingke <73776136+dmingke@users.noreply.github.com>
2023-02-11 15:49:02 -0500

    Update post.jsx

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import moment from "moment";
 
 
 // The parameter of this function is an object with a string called url inside it.
@@ -10,6 +12,10 @@ export default function Post({ url }) {
 
   const [imgUrl, setImgUrl] = useState("");
   const [owner, setOwner] = useState("");
+  const [created, setTime] = useState("");
+  // const [ourl, setOurl] = useState("");
+  const [oimg, setOimg] = useState("");
+  const [purl, setPost] = useState("");
 
 
   useEffect(() => {
@@ -27,27 +33,85 @@ export default function Post({ url }) {
         // If ignoreStaleRequest was set to true, we want to ignore the results of the
         // the request. Otherwise, update the state to trigger a new render.
         if (!ignoreStaleRequest) {
-          setImgUrl(data.imgUrl);
-          setOwner(data.owner);
+          setImgUrl(data.results.imgUrl);
+          setOwner(data.results.owner);
+          setTime(data.results.created);
+          // setOurl(data.results.ourl);
+          setOimg(data.results.oimg);
+          setPost(data.results.purl);
         }
       })
       .catch((error) => console.log(error));
 
 
     return () => {
-      // This is a cleanup function that runs whenever the Post component
-      // unmounts or re-renders. If a Post is about to unmount or re-render, we
-      // should avoid updating state.
       ignoreStaleRequest = true;
     };
   }, [url]);
 
-
+  // let time = moment(created).fromNow();
+  // let likes = <Moment=>
   // Render post image and post owner
   return (
-    <div className="post">
+    <div>
       <img src={imgUrl} alt="post_image" />
       <p>{owner}</p>
+      {/* <img src={ourl} alt = "" /> */}
+      <img src={oimg} alt = "" />
+      <strong><a href={purl}>{created}</a></strong>
+    </div>
+  );
+}
+
+function componentDidMount(props) {
+  const [state, setState] = useState({next:'', results:''});
+  fetch(props.url, { credentials: 'same-origin' })
+  .then((response) => {
+    if (!response.ok) throw Error(response.statusText);
+    return response.json();
+  })
+  .then((data) => {
+    setState({
+      next: data.next,
+      results: data.results,
+    });
+  })
+  .catch(error => console.log(error));
+
+
+const posts = []
+for (const [index, post] of state.results.entries()) {
+  posts.push(<Post url={post.url} postid={post.postid} key={index} />)
+}
+
+function fetchData() {
+  fetch(state.next, { credentials: 'same-origin' })
+  .then((response) => {
+    if (!response.ok) throw Error(response.statusText);
+    return response.json();
+  })
+  .then((data) => {
+    this.setState(prevState => ({
+      next: data.next,
+      results: prevState.results.concat(data.results),
+    }));
+  })
+  .catch(error => console.log(error)); 
+};
+  return(
+    <div className="container">
+      <InfiniteScroll 
+      dataLength={state.results.length} //This is important field to render the next data
+      next={fetchData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+        </p>
+      }
+      >
+    {posts}
+    </InfiniteScroll>
     </div>
   );
 }
