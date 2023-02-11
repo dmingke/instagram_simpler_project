945347844174da85bb8ae20247534bdeacd47b1e
yangqs6 <87424645+yangqs6@users.noreply.github.com>
2023-02-11 16:58:39 -0500

    Update post.jsx

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,68 +1,58 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import PostList from "./postlist";
+// import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
 
 
-// The parameter of this function is an object with a string called url inside it.
-// url is a prop for the Post component.
-export default function Posts({ url }) {
-  /* Display image and post owner of a single post */
+// // The parameter of this function is an object with a string called url inside it.
+// // url is a prop for the Post component.
+// export default function Post({ url }) {
+//   /* Display image and post owner of a single post */
 
 
 //   const [imgUrl, setImgUrl] = useState("");
 //   const [owner, setOwner] = useState("");
-    const [next,setNext] = useState("")
-    const [results,setResults] = useState([])
 
 
-
-  useEffect(() => {
-    // Declare a boolean flag that we can use to cancel the API request.
-    let ignoreStaleRequest = false;
-    // Call REST API to get the post's information
-    fetch(url, { credentials: "same-origin" })
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((data) => {
-        // If ignoreStaleRequest was set to true, we want to ignore the results of the
-        // the request. Otherwise, update the state to trigger a new render.
-        console.log(data)
-        if (!ignoreStaleRequest) {
-        //   setImgUrl(data.imgUrl);
-        //   setOwner(data.owner);
-            setNext(data.next);
-            setResults(data.results);
-        }
-        console.log("fine")
-        console.log(data.results)
-        console.log(data.results[0])
-        console.log(data.results[0].postid)
-        
-        
-      })
-      .catch((error) => console.log(error));
+//   useEffect(() => {
+//     // Declare a boolean flag that we can use to cancel the API request.
+//     let ignoreStaleRequest = false;
 
 
-    return () => {
-      // This is a cleanup function that runs whenever the Post component
-      // unmounts or re-renders. If a Post is about to unmount or re-render, we
-      // should avoid updating state.
-      ignoreStaleRequest = true;
-    };
-  }, [url]);
+//     // Call REST API to get the post's information
+//     fetch(url, { credentials: "same-origin" })
+//       .then((response) => {
+//         if (!response.ok) throw Error(response.statusText);
+//         return response.json();
+//       })
+//       .then((data) => {
+//         // If ignoreStaleRequest was set to true, we want to ignore the results of the
+//         // the request. Otherwise, update the state to trigger a new render.
+//         if (!ignoreStaleRequest) {
+//           setImgUrl(data.imgUrl);
+//           setOwner(data.owner);
+//         }
+//       })
+//       .catch((error) => console.log(error));
 
-    return (
-        <PostList postList={results}></PostList>
-        // <div>
-        //     {results}
-        // </div>
-        // <div>
-        //     Hello, I got the message.
-        // </div>
-    )
-   
-        
+
+//     return () => {
+//       // This is a cleanup function that runs whenever the Post component
+//       // unmounts or re-renders. If a Post is about to unmount or re-render, we
+//       // should avoid updating state.
+//       ignoreStaleRequest = true;
+//     };
+//   }, [url]);
+
+
+//   // Render post image and post owner
+//   return (
+//     <div className="post">
+//       <img src={imgUrl} alt="post_image" />
+//       <p>{owner}</p>
+//     </div>
 //   );
-}
+// }
+
+
+// Post.propTypes = {
+//   url: PropTypes.string.isRequired,
+// };
