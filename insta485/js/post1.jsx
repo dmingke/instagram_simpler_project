4287eae467ce5a3fafd631698ee782ac120fc390
yangqs6 <87424645+yangqs6@users.noreply.github.com>
2023-02-11 17:04:17 -0500

    Update post1.jsx

--- insta485/js/post1.jsx
+++ insta485/js/post1.jsx
@@ -5,8 +5,8 @@ import PropTypes from "prop-types";
 export default function Post1({post1}){
     return(
         <div>
-            <p>post1.id</p>
-            <p>post1.url</p>
+            <p>{post1.postid}</p>
+            <p>{post1.url}</p>
         </div>
         
     )
