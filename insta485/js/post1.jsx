deaf09f0c7419737cd56f640d441e9d6b8fcd874
yangqs6 <87424645+yangqs6@users.noreply.github.com>
2023-02-11 16:59:04 -0500

    Create post1.jsx

---
+++ insta485/js/post1.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+
+export default function Post1({post1}){
+    return(
+        <div>
+            <p>post1.id</p>
+            <p>post1.url</p>
+        </div>
+        
+    )
+}
