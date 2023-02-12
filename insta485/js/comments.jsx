4cd702be83a60e8bff260988e47ebb43ab8e8c7b
yangqs6 <87424645+yangqs6@users.noreply.github.com>
2023-02-11 22:34:23 -0500

    Create comments.jsx

---
+++ insta485/js/comments.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Comment from "./comment";
+
+
+export default function Comments({comments}){
+
+    return(
+        
+        comments.map(comment =>{
+            console.log(comment)
+            return <Comment key={comment.id} comment={comment}/>
+        })
+    )   
+}
