a0f1821ad75d2b667840e866543abc32dbc4ffab
dmingke <73776136+dmingke@users.noreply.github.com>
2023-02-13 21:03:30 -0500

    Update comment.jsx

--- insta485/js/comment.jsx
+++ insta485/js/comment.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function Comment({comment,changeComment}){
-  let comment_button;
-  function deleteComment(){
-    changeComment(comment.url)
-  }
-  if (comment.lognameOwnsThis){
-    comment_button = <button type="submit" onClick={deleteComment}>delete</button>
-  }
-  return(
-    <div>
-      <strong><a href={comment.ownerShowUrl}>{comment.owner}</a></strong>{comment.text}
-      {comment_button}
-    </div>
-  )
+export default function Comment({ comment, changeComment }) {
+    let comment_button;
+    function deleteComment() {
+        changeComment(comment.url)
+    }
+    if (comment.lognameOwnsThis) {
+        comment_button = <button type="submit" onClick={deleteComment}>delete</button>
+    }
+    return (
+        <div>
+            <strong><a href={comment.ownerShowUrl}>{comment.owner}</a></strong>{comment.text}
+            {comment_button}
+        </div>
+    )
 }
+
+Comment.propTypes = {
+    comment: PropTypes.object.isRequired,
+    changeComment: PropTypes.func,
+};
