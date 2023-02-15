04eb38737c212f89a01de08e3b348f81251863af
dmingke <73776136+dmingke@users.noreply.github.com>
2023-02-14 22:40:42 -0500

    Update post.jsx

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -13,28 +13,22 @@ export default function Posts({ url }) {
     const [results, setResult] = useState([]);
     const [hasMore, setHasMore] = useState(true);
     const [newResult, setNew] = useState([])
-    useEffect(() => {
+
       // Declare a boolean flag that we can use to cancel the API request.
-      let ignoreStaleRequest = false;
+
     fetch(url, { credentials: 'same-origin' })
     .then((response) => {
       if (!response.ok) throw Error(response.statusText);
       return response.json();
     })
     .then((data) => {
-      if (!ignoreStaleRequest) {
         setNext(data.next)
         setResult(data.results)
         setNew(data.results)
         }
-      })
+      )
     .catch(error => console.log(error));
-    return () => {
-      ignoreStaleRequest = true;
-    };
-    }, [url]);
-    
-   
+
   const fetchData = async() => {
       if (next === "") {
         setHasMore(false)
@@ -62,6 +56,7 @@ export default function Posts({ url }) {
       next={fetchData}
       hasMore={hasMore}
       loader={<h4>Loading...</h4>}
+      key="infinite"
       >
       <div>
         {newResult.map((result)=><Post key={result.postid} props = {result.url}/>)}
@@ -223,15 +218,20 @@ function Post({props}){
   function handleDelete(event){
     const deleteUrl = event.target.id;
     fetch(deleteUrl , { method: 'DELETE' })
-    .then((response) => {
-      if (!response.ok) throw Error(response.statusText);
-      return response.json();
-      })
-    .then((data) => {
-        setComments(data.comments)
-        setCommentsUrl(data.comments_url)
-    })
-    .catch(error => console.log(error));
+      .then(() => 
+      {
+          fetch(props, { credentials: 'same-origin' })
+          .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+          })
+          .then((data) => {
+              setComments(data.comments)
+              setCommentsUrl(data.comments_url)
+          })
+          .catch(error => console.log(error));
+
+      });
   }
 
   // var a =  Comments(){
@@ -260,6 +260,7 @@ function Post({props}){
         }
 
         function handleKeyDown(event){
+
             const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
